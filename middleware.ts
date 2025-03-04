2be39deda66ae8ce7acbc62b2c60ba4f636fec09
Safiur-lab <dos78819@gmail.com>
2025-03-04 10:08:37 +0530

    fixed middleware.ts file

--- middleware.ts
+++ middleware.ts
@@ -1,27 +1,16 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const publicRoutes = ['/', '/api/webhooks/clerk', '/api/webhooks/stripe'];
 
-export default clerkMiddleware(async (auth, req) => {
-  const { pathname } = new URL(req.url);
+const isPublicRoute = createRouteMatcher([
+  "/api/webhooks/clerk",
+]);
 
-  if (publicRoutes.includes(pathname)) {
-    return; // Skip middleware for public routes
+export default clerkMiddleware((_auth, req) => {
+  if (isPublicRoute(req)) {
+    return;
   }
-
-  const authObject = await auth();
-
-  if (!authObject.userId) {
-    // Redirect or handle unauthenticated users
-    return new Response("Unauthorized", { status: 401 });
-  }
-
-  // Proceed with the request
 });
 
 export const config = {
-  matcher: [
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    '/(api|trpc)(.*)',
-  ],
+  matcher: ["/((?!_next|.*\\..*).*)", "/(api|trpc)(.*)"],
 };
