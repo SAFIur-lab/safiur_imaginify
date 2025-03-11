fafa845a782d198b594c2db183cf62121f5f6a73
Safiur-lab <dos78819@gmail.com>
2025-03-11 11:06:02 +0530

    update pageprops

--- app/(Root)/transformation/Add/[type]/page.tsx
+++ app/(Root)/transformation/Add/[type]/page.tsx
@@ -2,54 +2,27 @@ import Header from '@/components/shared/Header';
 import React from 'react';
 import { transformationTypes } from '@/constant';
 import TransformationForm from '@/components/shared/TransformationForm';
-import { useEffect, useState } from 'react';
-import { useRouter, NextRouter } from 'next/router';
-import { useAuth } from '@clerk/nextjs';
+import { auth } from '@clerk/nextjs/server';
 import { getUserById } from '@/lib/actions/user.action';
+import { redirect } from 'next/navigation';
 
 interface PageProps {
-  params: { type: keyof typeof transformationTypes };
+  params: Promise<{ type: string }>; // Update to reflect that params is a Promise
 }
-const AddTransformationPageType = ({ params }: PageProps) => {
-  const { type } = params;
-  const router = useRouter() as NextRouter;
-  interface User {
-    _id: string;
-    creditBalance: number;
-    // Add other user properties if needed
+
+const AddTransformationPageType = async ({ params }: PageProps) => {
+  const { type } = await params; // Await the params to resolve
+  const { userId } = await auth();
+
+  if (!userId) redirect('/sign-in');
+
+  const transformation = transformationTypes[type as TransformationTypeKey];
+
+  if (!transformation) {
+    redirect('/error'); // Redirect if the transformation type is invalid
   }
-  
-  const [user, setUser] = useState<User | null>(null);
-  const [transformation, setTransformation] = useState<typeof transformationTypes[keyof typeof transformationTypes] | null>(null);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const { userId } = await useAuth();
-
-      if (!userId) {
-        router.push('/sign-in');
-        return;
-      }
-
-      const transformation = transformationTypes[type as TransformationTypeKey];
-
-      if (!transformation) {
-        router.push('/error'); // Redirect if the transformation type is invalid
-        return;
-      }
-
-      const user = await getUserById(userId);
-      setUser(user);
-      setTransformation(transformation);
-      fetchData();
-    };
-
-    fetchData();
-  }, [type, router]);
-
-  if (!user || !transformation) {
-    return <div>Loading...</div>;
-  }
+  const user = await getUserById(userId);
 
   return (
     <>
@@ -63,6 +36,5 @@ const AddTransformationPageType = ({ params }: PageProps) => {
     </>
   );
 };
-
 
 export default AddTransformationPageType;
