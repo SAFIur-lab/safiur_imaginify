d6c5e58b7aafb708042dc26400f248ec0869ae80
Safiur-lab <dos78819@gmail.com>
2025-03-13 11:30:45 +0530

    clerk webhooks

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,27 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-
+// Define public routes
 const isPublicRoute = createRouteMatcher([
   "/api/webhooks/clerk",
 ]);
 
-export default clerkMiddleware((_auth, req) => {
+// Middleware to protect routes
+export default clerkMiddleware(async (auth, req) => {
+  // Allow public routes without authentication
   if (isPublicRoute(req)) {
     return;
   }
+  
+  // Protect all other routes
+  await auth.protect();
 });
 
+// Configuration for the middleware
 export const config = {
-  matcher: ["/((?!_next|.*\\..*).*)", "/(api|trpc)(.*)"],
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
 };
